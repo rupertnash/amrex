2483aef7d5c2199aa1f85a106859a94aec5b66b8
Travis CI <atmyers@lbl.gov>
2019-01-31 21:15:02 +0000

    Deploy to GitHub Pages: 26d8f6a5a421b9eb3ba06bb4a06eba1194aac9fa

--- docs_html/doxygen/search/variables_14.js
+++ docs_html/doxygen/search/variables_14.js
@@ -2,10 +2,10 @@ var searchData=
 [
   ['unit',['Unit',['../classamrex_1_1_int_vect.html#a93df7e94c6d2d07ea3eb7647500bf751',1,'amrex::IntVect::Unit()'],['../classamrex_1_1_real_vect.html#a6745f136d89bdb15e57c3387ca01e973',1,'amrex::RealVect::Unit()']]],
   ['unknown',['unknown',['../classamrex__eb2__2d__moudle.html#a2d75370da22b568f5949f494a03aaf80',1,'amrex_eb2_2d_moudle::unknown()'],['../classamrex__eb2__3d__module.html#a3b404a78568dcaa7775bee49c5e8fe18',1,'amrex_eb2_3d_module::unknown()']]],
-  ['use_5fcuda_5faware_5fmpi',['use_cuda_aware_mpi',['../namespaceamrex_1_1_parallel_descriptor.html#abf1f8b11d69baab03a99dcca8cbd4ca1',1,'amrex::ParallelDescriptor']]],
   ['use_5fefficient_5fregrid',['use_efficient_regrid',['../namespaceamrex.html#af1ff4a567f8d0cdc485062507aac9742',1,'amrex']]],
   ['use_5ffixed_5fcoarse_5fgrids',['use_fixed_coarse_grids',['../classamrex_1_1_amr_mesh.html#a3162b5c3697f4ce32d585ca434ac40a5',1,'amrex::AmrMesh']]],
   ['use_5ffixed_5fupto_5flevel',['use_fixed_upto_level',['../classamrex_1_1_amr_mesh.html#a15a5406d39363635d73cc1d986546fe7',1,'amrex::AmrMesh']]],
+  ['use_5fgpu_5faware_5fmpi',['use_gpu_aware_mpi',['../namespaceamrex_1_1_parallel_descriptor.html#af122c95310b715f9006b650405e56bc8',1,'amrex::ParallelDescriptor']]],
   ['use_5fmask',['use_mask',['../classamrex_1_1_neighbor_particle_container.html#a21bd4a08b87b0afeeb0d8889c619c86c',1,'amrex::NeighborParticleContainer']]],
   ['use_5fnew_5fchop',['use_new_chop',['../classamrex_1_1_amr_mesh.html#abaf8432668e3b350306a62e8662f4f96',1,'amrex::AmrMesh']]],
   ['use_5fphierr',['use_phierr',['../classamrex_1_1_l_s_core_base.html#af4ed8d84ded37119725a657db8b99d85',1,'amrex::LSCoreBase']]],
