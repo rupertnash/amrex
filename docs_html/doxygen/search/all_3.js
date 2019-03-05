8669cad8635ab93d916b71a6c489ef6449c14d84
Travis CI <atmyers@lbl.gov>
2019-03-05 23:45:30 +0000

    Deploy to GitHub Pages: 06a6419f9bd8a799c5a84a4fd603296b9cbe83fa

--- docs_html/doxygen/search/all_3.js
+++ docs_html/doxygen/search/all_3.js
@@ -81,7 +81,7 @@ var searchData=
   ['cfinfo',['CFinfo',['../structamrex_1_1FabArrayBase_1_1CFinfo.html#a2ad23c41fccd032693997df69c0bce0d',1,'amrex::FabArrayBase::CFinfo']]],
   ['cfinfocache',['CFinfoCache',['../classamrex_1_1FabArrayBase.html#ab93444c8298b6446b699b33cf6c9247b',1,'amrex::FabArrayBase']]],
   ['cfinfocacheiter',['CFinfoCacheIter',['../classamrex_1_1FabArrayBase.html#ae15d8b6b473748103771433c507773ad',1,'amrex::FabArrayBase']]],
-  ['cg',['cg',['../classamrex_1_1MLMG.html#aca2027687047f1254d8a103beee1e4dfa6e9cf3eef65da697796cf33f27eb0f57',1,'amrex::MLMG::cg()'],['../classamrex_1_1CGSolver.html#a0c6d6797999b216f26492f170891497ba43998d5e117bedcd91f4c6045307cba7',1,'amrex::CGSolver::CG()'],['../classamrex_1_1MLCGSolver.html#ac7b947856c5f3a21769938261be5d521a5202c6586cac8bee468e86d1ff854231',1,'amrex::MLCGSolver::CG()']]],
+  ['cg',['CG',['../classamrex_1_1CGSolver.html#a0c6d6797999b216f26492f170891497ba43998d5e117bedcd91f4c6045307cba7',1,'amrex::CGSolver::CG()'],['../classamrex_1_1MLCGSolver.html#ac7b947856c5f3a21769938261be5d521a5202c6586cac8bee468e86d1ff854231',1,'amrex::MLCGSolver::CG()'],['../classamrex_1_1MLMG.html#aca2027687047f1254d8a103beee1e4dfa6e9cf3eef65da697796cf33f27eb0f57',1,'amrex::MLMG::cg()']]],
   ['cg_5fsolver',['cg_solver',['../classamrex_1_1CGSolver.html#a4af7811a9f5617ba318f3b37cc160718',1,'amrex::CGSolver']]],
   ['cgsolver',['CGSolver',['../classamrex_1_1CGSolver.html#ae8bd9022532e30643d758bdc1cdc134b',1,'amrex::CGSolver::CGSolver(LinOp &amp;_lp, bool _use_mg_precond=false, int _lev=0)'],['../classamrex_1_1CGSolver.html#aca325b5d06e29897bb9e4aec580abbf8',1,'amrex::CGSolver::CGSolver(const CGSolver &amp;)']]],
   ['cgsolver',['CGSolver',['../classamrex_1_1CGSolver.html',1,'amrex']]],
